6e776f1541db43bcc9fa43af999fb9c839a2eb41
tahir hussain <tahirhussain2257@gmail.com>
2023-04-04 16:57:31 +0100

    completed

---
+++ collections_task1.py
@@ -0,0 +1,31 @@
+# Task 1.1
+
+milli_list = ["supercar", "yacht", "jet", "mansion", "island"]
+
+# print list and check type
+print(milli_list)
+print(type(milli_list))
+
+# print first second and last element
+print(milli_list[0])
+print(milli_list[1])
+print(milli_list[-1])
+
+# replace first and another item in list and print
+milli_list[0] = "Audi"
+print(milli_list)
+
+milli_list[3] = "beach house"
+print(milli_list)
+
+# add and remove new item to list and print
+
+milli_list.append("Tiger")
+print(milli_list)
+
+milli_list.remove("Audi")
+print(milli_list)
+
+milli_list.pop()
+print(milli_list)
+
